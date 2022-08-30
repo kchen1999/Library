336a1247c48e4f53da2123911c5157cb963c225c
kchen1999 <kchen174175@gmail.com>
2022-08-30 20:51:51 +1000

    added new book button

--- script.js
+++ script.js
@@ -17,29 +17,29 @@ function Book(title, author, pages, isRead) {
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
+    const libraryContainer = document.querySelector('.library-container');
+    const card = document.createElement('div');
+    const title = document.createElement('p');
+    const author = document.createElement('p');
+    const pages = document.createElement('p');
+    card.classList.add('card');
+    title.textContent = book.title;
+    author.textContent = book.author;
+    pages.textContent = book.pages + ' pages';
+    card.append(title);
+    card.append(author);
+    card.append(pages);
+    libraryContainer.append(card);
 }
 
-function displayLibrary() {
-    myLibrary.forEach(book => {
-        const libraryContainer = document.querySelector('.library-container'); 
-        const card = document.createElement('div');
-        const title = document.createElement('p');
-        const author = document.createElement('p');
-        const pages = document.createElement('p');
-        card.classList.add('card'); 
-        title.textContent = book.title; 
-        author.textContent = book.author;
-        pages.textContent = book.pages; 
-        card.append(title);
-        card.append(author);
-        card.append(pages);
-        libraryContainer.append(card); 
-    });
-}
 
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
 const harryPotter = new Book('Chamber of Secrets', 'J.K Rowling', 800, true);
+const windsOfWinter = new Book('Winds of Winter', 'George R.R Martin', 1200, false);
+const deltoraQuest = new Book('The Forests of Silence', 'Emily Rodda', 400, true);
 addBookToLibrary(theHobbit);
 addBookToLibrary(harryPotter);
-displayLibrary();
+addBookToLibrary(windsOfWinter);
+addBookToLibrary(deltoraQuest);
+
 
