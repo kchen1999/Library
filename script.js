0abd62b0b64996c19eaefd09bf5df26704d61c54
kchen1999 <kchen174175@gmail.com>
2022-09-04 03:54:49 +1000

    :q added remove book button

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let myLibrary = [];
 
-function Book(title, author, pages, isRead) {
+function Book(title, author, pages, isRead, index) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.isRead = isRead; 
+    this.index = index; 
     this.info = function() {
         if(this.isRead === "true") {
             return this.title + " by " + this.author + ", " + this.pages + " pages, has read yet";
@@ -27,15 +28,28 @@ function displayLibrary() {
         const title = document.createElement('p');
         const author = document.createElement('p');
         const pages = document.createElement('p');
+        const removeBtn = document.createElement('button');
         card.classList.add('card');
+        removeBtn.classList.add('remove-btn');
         title.textContent = book.title;
         author.textContent = book.author;
         pages.textContent = book.pages + ' pages';
+        removeBtn.textContent = "Remove";
         card.append(title);
         card.append(author);
         card.append(pages);
-        libraryContainer.append(card);
+        card.append(removeBtn);
+        libraryContainer.append(card); 
+        removeBtn.addEventListener('click', () => {
+            myLibrary.splice(book.index, 1);
+            for(let i = book.index; i < myLibrary.length; i++) {
+                myLibrary[i].index -= 1; 
+            }
+            libraryContainer.removeChild(card); 
+        });
     });
+
+
 }
 
 
@@ -50,7 +64,7 @@ submitBtn.addEventListener('click', () => {
     const author = document.getElementById('author').value;
     const pages = document.getElementById('pages').value;
     const isRead = document.getElementById('is-read').checked;
-    const newBook = new Book(title, author, pages, isRead);
+    const newBook = new Book(title, author, pages, isRead, myLibrary.length);
     modal.style.display = 'none';
     addBookToLibrary(newBook);
     displayLibrary();
@@ -59,10 +73,11 @@ submitBtn.addEventListener('click', () => {
 
 
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
-const harryPotter = new Book('Chamber of Secrets', 'J.K Rowling', 800, true);
-const windsOfWinter = new Book('Winds of Winter', 'George R.R Martin', 1200, false);
-const deltoraQuest = new Book('The Forests of Silence', 'Emily Rodda', 400, true);
+
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false, 0);
+const harryPotter = new Book('Chamber of Secrets', 'J.K Rowling', 800, true, 1);
+const windsOfWinter = new Book('Winds of Winter', 'George R.R Martin', 1200, false, 2);
+const deltoraQuest = new Book('The Forests of Silence', 'Emily Rodda', 400, true, 3);
 addBookToLibrary(theHobbit);
 addBookToLibrary(harryPotter);
 addBookToLibrary(windsOfWinter);
